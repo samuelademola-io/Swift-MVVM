461f902ba7b1e915195d859f8322a1eb75b71aa5
Samuel F. Ademola <zion@Ebenezer.local>
2020-11-18 09:00:59 -0600

    1st  commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Samuel F. Ademola on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Swift-MVVM
+Understanding the MVVM Architecture and how data passes through each layer of SwiftUI
